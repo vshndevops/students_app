94c82cd8809e423ec55d765247dd5fccb64a444b
Abdul Rasheed Pariseri <rasheed@Abduls-MacBook-Pro.local>
2023-06-19 18:38:48 +0530

    code updated

--- iac/network.tf
+++ iac/network.tf
@@ -7,3 +7,29 @@ resource "aws_vpc" "devops" {
   }
 
 }
+
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.devops.id
+  cidr_block        = "172.16.0.0/26"
+  availability_zone = "us-east-1a"
+
+}
+
+# Internet gateway
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devops.id
+
+}
+
+
+resource "aws_route_table" "rt1" {
+  vpc_id = aws_vpc.devops.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+}
