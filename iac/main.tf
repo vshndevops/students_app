f8c6388abfc1219c0fab15cc72569630448d9eca
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-07-15 08:36:17 +0530

    Public IP

--- iac/main.tf
+++ iac/main.tf
@@ -19,6 +19,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-053b0d53c279acc90" # us-east-1a
   instance_type = "t2.micro"
   key_name = "lab-key"
+  subnet_id     = aws_subnet.subnet.id
   associate_public_ip_address = true
 
   # network_interface {
