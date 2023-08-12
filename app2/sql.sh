5525e9184e3310da5d91ea6ef81fc939dfdc4d1c
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-08-12 11:04:21 +0530

    teachers app

--- app2/sql.sh
+++ app2/sql.sh
@@ -4,7 +4,7 @@ export USER=admin
 export PASS=Asdfg1234$%
 
 # Login to MySQL
-mysql -u$USER -p$PASS -h students.cyxbau1cwuxn.us-east-1.rds.amazonaws.com << EOF
+mysql -u$USER -p$PASS -h schoolapp.cyxbau1cwuxn.us-east-1.rds.amazonaws.com << EOF
 
 CREATE DATABASE teachers;
 USE teachers;
