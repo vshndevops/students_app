9d2b36d504f28980d3c98a5d86ed54790022e283
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-08-12 07:12:35 +0530

    db script

--- app/sql.sh
+++ app/sql.sh
@@ -8,13 +8,13 @@ mysql -u$USER -p$PASS -h students.cyxbau1cwuxn.us-east-1.rds.amazonaws.com << EO
 
 CREATE DATABASE teachers;
 USE teachers;
-CREATE TABLE IF NOT EXISTS `teachers` (
-    `id` int(5) NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `address` varchar(255) NOT NULL,
-    `mobile` int(15) NOT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS teachers (
+    id int(5) NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    address varchar(255) NOT NULL,
+    mobile int(15) NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 EOF
