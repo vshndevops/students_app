c9980d322ba836f53cb6af8a0525119ba74cd1a9
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-08-12 06:53:54 +0530

    db script

--- app/sql.sh
+++ app/sql.sh
@@ -7,10 +7,23 @@ export PASS=Asdfg1234$%
 mysql -u$USER -p$PASS -h students.cyxbau1cwuxn.us-east-1.rds.amazonaws.com << EOF
 
 
-USE students;
+# Create database
+CREATE DATABASE socka;
 
-INSERT INTO `students` (`name`,`email`,`address`,`mobile`)
-VALUES
-  ("rasheedmon", "rasheedmon@gmail.com", 'Chettippadi',96565);
+
+# Use the database
+USE socka;
+
+# Create table
+CREATE TABLE players (
+  id int(5) NOT NULL AUTO_INCREMENT,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  position varchar(255) NOT NULL,
+  number int(11) NOT NULL,
+  image varchar(255) NOT NULL,
+  user_name varchar(20) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 EOF
