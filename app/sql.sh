7be284e4535e1a729dd1617eeff7aacb48457675
Abdul Rasheed Pariseri <rasheed@abduls-mbp.lan>
2023-08-12 06:58:20 +0530

    db script

--- app/sql.sh
+++ app/sql.sh
@@ -7,23 +7,15 @@ export PASS=Asdfg1234$%
 mysql -u$USER -p$PASS -h students.cyxbau1cwuxn.us-east-1.rds.amazonaws.com << EOF
 
 
-# Create database
-CREATE DATABASE socka;
+USE students;
 
-
-# Use the database
-USE socka;
-
-# Create table
-CREATE TABLE players (
-  id int(5) NOT NULL AUTO_INCREMENT,
-  first_name varchar(255) NOT NULL,
-  last_name varchar(255) NOT NULL,
-  position varchar(255) NOT NULL,
-  number int(11) NOT NULL,
-  image varchar(255) NOT NULL,
-  user_name varchar(20) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS `teacher` (
+    `id` int(5) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `address` varchar(255) NOT NULL,
+    `mobile` int(15) NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 EOF
