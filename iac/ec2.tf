ccdb4b32381cc637480d9be23783d2e3936de234
Mufeed <mufeed@Mvalappil01.clinipace.net>
2023-08-16 14:15:14 +0530

    ec2

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
   ami           = "ami-053b0d53c279acc90" # us-east-1a
   instance_type = "t2.micro"
-  key_name = "lab-key"
+  key_name = "test"
   subnet_id     = aws_subnet.subnet.id
   associate_public_ip_address = true
   vpc_security_group_ids= [aws_security_group.myapp_sg.id]
