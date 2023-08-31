0cc88a7a09f7e4612d0caee8b712d9409605d6d1
root <root@ip-172-31-10-102.ap-south-1.compute.internal>
2023-08-31 11:05:30 +0000

    commit2

--- iac/ec2.tf
+++ iac/ec2.tf
@@ -4,9 +4,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-053b0d53c279acc90" # us-east-1a
+  ami           = "ami-0f5ee92e2d63afc18" # us-east-1a
   instance_type = "t2.micro"
-  key_name = "test"
+  key_name = "Jenkinsproject"
   subnet_id     = aws_subnet.subnet.id
   associate_public_ip_address = true
   vpc_security_group_ids= [aws_security_group.myapp_sg.id]
